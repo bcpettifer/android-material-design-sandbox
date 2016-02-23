8b0b34901a6d8c5f82f3cae872fca6cff7b560a5
benpdev <benpdev+github@gmail.com>
2016-02-23 23:24:31 +0000

    Added initial project with simple layout

---
+++ App/src/main/java/com.example.android.materialdesigncodelab/fragments/TileContentFragment.java
@@ -0,0 +1,20 @@
+package com.example.android.materialdesigncodelab.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.materialdesigncodelab.R;
+
+/**
+ * Provides UI for the view with Cards.
+ */
+public class TileContentFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.item_tile, null);
+    }
+}
